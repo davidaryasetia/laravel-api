fe3ebeeb188f18a95b97a9b4b84fab3089218a5a
David Aryasetia <david.aryasetia@gmail.com>
2024-06-24 00:48:30 +0700

    backend crud api post article

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,13 +6,14 @@ use App\Http\Resources\PostDetailResource;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     public function index()
     {
         $posts = Post::all();
-        return PostResource::collection($posts);
+        return PostDetailResource::collection($posts->loadMissing('writer:id,username'));
     }
 
     public function show($id)
@@ -21,10 +22,38 @@ class PostController extends Controller
         return new PostDetailResource($post); 
     }
 
-    public function show2($id)
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|max:255', 
+            'news_content' => 'required'
+        ]); 
+
+        $request['author'] = Auth::user()->id;
+        $post = Post::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'title' => 'required|max:255', 
+            'news_content' => 'required'
+        ]);
+
+        $post = Post::findOrFail($id);
+        $post->update($request->all());
+
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
+    }
+
+    public function destroy($id)
     {
         $post = Post::findOrFail($id);
-        return new PostDetailResource($post);
+        $post->delete();
+
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
     }
 
 }
+
