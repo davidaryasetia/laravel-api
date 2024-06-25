9d15249cb1a1fb415289e877365a526fde320067
David Aryasetia <david.aryasetia@gmail.com>
2024-06-25 09:19:43 +0700

    finalize API article project

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,21 +5,23 @@ namespace App\Http\Controllers;
 use App\Http\Resources\PostDetailResource;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
+use Faker\Core\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
     public function index()
     {
         $posts = Post::all();
-        return PostDetailResource::collection($posts->loadMissing('writer:id,username'));
+        return PostDetailResource::collection($posts->loadMissing('writer:id,username','comments:id,post_id,user_id,comments_content'));
     }
 
     public function show($id)
     {
         $post = Post::with('writer:id,username')->findOrFail($id);
-        return new PostDetailResource($post); 
+        return new PostDetailResource($post->loadMissing(['writer:id,username','comments:id,post_id,comments_content'])); 
     }
 
     public function store(Request $request)
@@ -29,6 +31,17 @@ class PostController extends Controller
             'news_content' => 'required'
         ]); 
 
+        $image = '';
+        if($request->file){
+            // disini untuk upload file 
+            $fileName = $this->generateRandomString();
+            $extension = $request->file->extension();
+            $image = $fileName.'.'.$extension;
+
+            Storage::putFileAs('image', $request->file, $fileName.'.'.$extension);
+        }
+
+        $request['image'] = $image;
         $request['author'] = Auth::user()->id;
         $post = Post::create($request->all());
         return new PostDetailResource($post->loadMissing('writer:id,username'));
@@ -55,5 +68,14 @@ class PostController extends Controller
         return new PostDetailResource($post->loadMissing('writer:id,username'));
     }
 
+    function generateRandomString($length = 30) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
 
